8fd86f839a902de3b6fe99a7010270fbf6c5ba8d
Dan Sexton <dsexton702@gmail.com>
2014-03-29 13:59:44 -0700

    Added lots
    Phone sound control
    Deprecated code for live preview of camera
    other stuf

--- src/APPMESSAGE.c
+++ src/APPMESSAGE.c
@@ -13,11 +13,33 @@
 
  void in_received_handler(DictionaryIterator *received, void *context) {
  
+ Tuple *text_tuple = dict_find(received, SPY_KEY_STOP);
+   Tuple *get_tuple = dict_find(received, SPY_KEY_START);
 
-  }
+        
+    if(text_tuple){
+      
+      datar = text_tuple->value->uint8;
+      
+  //     snprintf(text_buffer, 1024, "%s", text_tuple->value->cstring);
+  
+
+           //APP_LOG(APP_LOG_LEVEL_DEBUG, "Text: %s", text_buffer);
+}
+
+   if(get_tuple){
+       snprintf(text_buffer, 1024, "%s", get_tuple->value->cstring);
+  text_layer_set_text(text_layer, text_buffer);
+
+           APP_LOG(APP_LOG_LEVEL_DEBUG, "Text: %s", text_buffer);
+}
+      
+
+    
+  
 
 	
-
+ }
          
       
 
--- src/SPY.c
+++ src/SPY.c
@@ -3,15 +3,19 @@
 
 
 
-#define NUM_FIRST_MENU_ITEMS 7
+#define NUM_FIRST_MENU_ITEMS 8
 #define NUM_MENU_SECTIONS 1
-
+static AppSync sync;
 static Window *window;
 static AppTimer *timerz;
+static Layer *layer;
 static AppTimer *timers;
 static AppTimer *timer;
 Layer *window_layers;
+static GBitmap *image;
 Layer *window_layer;
+static AppSync sync;
+static uint8_t sync_buffer[1024];
 static TextLayer *text_layer;
 // This is a menu layer
 // You have more control than with a simple menu layer
@@ -21,6 +25,10 @@ static MenuLayer *menu_layer;
 
 
 static int current_icon = 0;
+enum WeatherKey {
+  WEATHER_ICON_KEY = 0x1  // TUPLE_CSTRING
+};
+
 
 // You can draw arbitrary things in a menu item such as a background
 static GBitmap *menu_background;
@@ -40,6 +48,21 @@ static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t secti
     
   
   
+}
+
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+  switch (key) {
+    case WEATHER_ICON_KEY:
+     
+    // createImg(datar = new_tuple->value->uint32);
+      text_layer_set_text(text_layer, new_tuple->value->cstring);
+      break;
+  }
 }
 
 
@@ -84,7 +107,7 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
           break;
 
         case 2:
-                  menu_cell_basic_draw(ctx, cell_layer, "Audi Recording", NULL, NULL);
+                  menu_cell_basic_draw(ctx, cell_layer, "Audio Recording", NULL, NULL);
           break;
 case 3:
                   menu_cell_basic_draw(ctx, cell_layer, "Voice Recognition", NULL, NULL);
@@ -98,6 +121,10 @@ case 5:
 		  
 		  case 6:
                   menu_cell_basic_draw(ctx, cell_layer, "Front Video Cam", NULL, NULL);
+          break;
+        
+         case 7:
+                  menu_cell_basic_draw(ctx, cell_layer, "Silence Phone", NULL, NULL);
           break;
       
       }
@@ -121,6 +148,18 @@ app_message_outbox_begin(&iter);
 
 
 
+static void timer_modz(){
+Tuplet initial_values[] = {
+    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1)};
+  app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
+      sync_tuple_changed_callback, sync_error_callback, NULL);
+
+
+}
+
+static void timer_modze(){
+   createImg(datar);
+}
 
 static void timer_mods(){
 app_timer_cancel(timerz);
@@ -133,6 +172,7 @@ app_timer_cancel(timer);
 }
 
 static void timer_mod(){
+  
 
 text_layer_set_text(text_layer, "Picture Taken");
 
@@ -179,16 +219,23 @@ window_sets(window, man);
 
 static bool audi = false;
 static bool flash = false;
-int symbol = 1;
-int symbols = 2;
-int symbolz = 3;
-int sign = 4;
-int signs = 5;
-int signz = 6;
-int sigs = 7;
+
 // Here we capture when a user selects a menu item
 void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
-
+  Tuplet initial_values[] = {
+    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1)};
+  app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
+      sync_tuple_changed_callback, sync_error_callback, NULL);  
+  
+register  int symbol = 1;
+register int symbols = 2;
+register int symbolz = 3;
+register int sign = 4;
+register int signs = 5;
+register int signz = 6;
+register int sigs = 7;
+register int sgis = 8;
+  
      register Tuplet symbol_tuple = TupletInteger(SPY_KEY_START, symbol);
      register Tuplet symbol_tuples = TupletInteger(SPY_KEY_START, symbols);
      register Tuplet symbol_tuplez = TupletInteger(SPY_KEY_START, symbolz);
@@ -196,20 +243,23 @@ void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *da
      register Tuplet symbol_tupz = TupletInteger(SPY_KEY_START, signs);
      register Tuplet symbol_tupez = TupletInteger(SPY_KEY_START, signz);
 	  register Tuplet symbol_tup = TupletInteger(SPY_KEY_START, sigs);
+  	 register Tuplet symbol_tuppz = TupletInteger(SPY_KEY_START, sgis);
+
   switch (cell_index->row) {
 
 case 0:
 
 
 sendMes(symbol_tupez);
+  
  window_layers = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layers);
  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w-0, 200 } });
-  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
+ text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 text_layer_set_text(text_layer, "Loading Camera");
   layer_add_child(window_layers, text_layer_get_layer(text_layer));
-timerz = app_timer_register(3300 /* milliseconds */, timer_mod, NULL);
+timerz = app_timer_register(3500 /* milliseconds */, timer_mod, NULL);
 break;
 case 1:
 
@@ -221,8 +271,8 @@ window_layers = window_get_root_layer(window);
   text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 text_layer_set_text(text_layer, "Loading Camera");
-  layer_add_child(window_layers, text_layer_get_layer(text_layer));
-timerz = app_timer_register(3300 /* milliseconds */, timer_mod, NULL);
+ layer_add_child(window_layers, text_layer_get_layer(text_layer));
+   timerz = app_timer_register(3500 /* milliseconds */, timer_mod, NULL);
     // This is the menu item with the cycling icon
 break;
     case 2:
@@ -236,9 +286,8 @@ sendMes(symbol_tups);
  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounde.size.w-0, 200 } });
   text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-text_layer_set_text(text_layer, "Recording Started...");
-  layer_add_child(window_layers, text_layer_get_layer(text_layer));
-      
+        layer_add_child(window_layers, text_layer_get_layer(text_layer));
+
     }else
       if(audi == true){
       layer_destroy(text_layer_get_layer(text_layer));
@@ -249,9 +298,9 @@ text_layer_set_text(text_layer, "Recording Started...");
  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounde.size.w-0, 200 } });
   text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-text_layer_set_text(text_layer, "Recording stopped");
-  layer_add_child(window_layers, text_layer_get_layer(text_layer));
-            timer = app_timer_register(600 /* milliseconds */, timer_mods, NULL);
+        layer_add_child(window_layers, text_layer_get_layer(text_layer));
+
+        timer = app_timer_register(4000 /* milliseconds */, timer_mods, NULL);
     
     
     }
@@ -266,7 +315,7 @@ window_layers = window_get_root_layer(window);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 text_layer_set_text(text_layer, "Loading Voice Rec");
   layer_add_child(window_layers, text_layer_get_layer(text_layer));
-timer = app_timer_register(1000 /* milliseconds */, timer_mods, NULL);
+
 
 
       break;
@@ -279,7 +328,7 @@ case 4:
  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w-0, 200 } });
   text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-text_layer_set_text(text_layer, "Flashlight on");
+  
   layer_add_child(window_layers, text_layer_get_layer(text_layer));
   }else
     if(flash == true){
@@ -290,8 +339,7 @@ layer_destroy(text_layer_get_layer(text_layer));
  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w-0, 200 } });
   text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-text_layer_set_text(text_layer, "Flashlight off");
-  layer_add_child(window_layers, text_layer_get_layer(text_layer));
+
       timer = app_timer_register(600 /* milliseconds */, timer_mods, NULL);
 
   }
@@ -300,13 +348,94 @@ case 5:
 
 
   sendMes(symbol_tuple);
-      break;
+     if(audi == false){
+      audi = true;
+    window_layers = window_get_root_layer(window);
+  GRect bounde = layer_get_bounds(window_layers);
+ text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounde.size.w-0, 200 } });
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+       text_layer_set_text(text_layer, "Loading Recorder");
+layer_add_child(window_layers, text_layer_get_layer(text_layer));
+      
+    }else
+      if(audi == true){
+      layer_destroy(text_layer_get_layer(text_layer));
+
+      audi = false;
+      window_layers = window_get_root_layer(window);
+  GRect bounde = layer_get_bounds(window_layers);
+ text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounde.size.w-0, 200 } });
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+        timer = app_timer_register(600 /* milliseconds */, timer_mods, NULL);
+    
+    
+    }
+    
+    
+       break;
 	  
 	  case 6:
 
 
   sendMes(symbol_tup);
+     if(audi == false){
+      audi = true;
+    window_layers = window_get_root_layer(window);
+  GRect bounde = layer_get_bounds(window_layers);
+ text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounde.size.w-0, 200 } });
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+       text_layer_set_text(text_layer, "Loading Recorder");
+       layer_add_child(window_layers, text_layer_get_layer(text_layer));
+
+      
+    }else
+      if(audi == true){
+      layer_destroy(text_layer_get_layer(text_layer));
+
+      audi = false;
+      window_layers = window_get_root_layer(window);
+  GRect bounde = layer_get_bounds(window_layers);
+ text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounde.size.w-0, 200 } });
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+        timer = app_timer_register(600 /* milliseconds */, timer_mods, NULL);
+    
+     }
+    
       break;
+    
+    case 7:
+     sendMes(symbol_tuppz);
+     if(audi == false){
+      audi = true;
+    window_layers = window_get_root_layer(window);
+  GRect bounde = layer_get_bounds(window_layers);
+ text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounde.size.w-0, 200 } });
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+       
+layer_add_child(window_layers, text_layer_get_layer(text_layer));
+        timer = app_timer_register(2500 /* milliseconds */, timer_mods, NULL);
+      
+    }else
+      if(audi == true){
+      
+
+      audi = false;
+      window_layers = window_get_root_layer(window);
+  GRect bounde = layer_get_bounds(window_layers);
+ text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounde.size.w-0, 200 } });
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+       layer_add_child(window_layers, text_layer_get_layer(text_layer));
+
+        timer = app_timer_register(2500 /* milliseconds */, timer_mods, NULL);
+     }
+    
+    break;
 
 
   }
@@ -337,7 +466,7 @@ void window_load(Window *window) {
 
   // Create the menu layer
   menu_layer = menu_layer_create(bounds);
-
+   
   // Set all the callbacks for the menu layer
   menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
     .get_num_sections = menu_get_num_sections_callback,
@@ -353,10 +482,43 @@ void window_load(Window *window) {
 
   // Add it to the window for display
   layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
+  
+  
+}
+
+static void layer_update_callback(Layer *me, GContext* ctx) {
+  
+   GRect bounds = layer_get_frame(me);
+   graphics_draw_bitmap_in_rect(ctx, image, (GRect) { .origin = { 0, 0 }, .size = bounds.size });
+
+  graphics_draw_bitmap_in_rect(ctx, image, (GRect) { .origin = { 100, 100 }, .size = bounds.size });
+}
+ 
+
+void createImg(uint8_t data){
+ Layer *window_layerz = window_get_root_layer(window);
+   GRect bounds = layer_get_frame(window_layerz);
+      image = gbitmap_create_with_data(&data);
+
+ 
+
+
+  
+ image_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(image_layer, image);
+  bitmap_layer_set_alignment(image_layer, GAlignCenter);
+    layer_set_update_proc(window_layerz, layer_update_callback);
+  layer_add_child(window_layerz, bitmap_layer_get_layer(image_layer));
+  
+  
+  
+  
 }
 
 void window_unload(Window *window) {
   // Destroy the menu layer
+  
+  app_sync_deinit(&sync); 
   menu_layer_destroy(menu_layer);
 app_timer_cancel(timerz);
   // Cleanup the menu icons
@@ -365,6 +527,16 @@ app_timer_cancel(timerz);
 
 int main(void) {
   window = window_create();
+  app_comm_set_sniff_interval(SNIFF_INTERVAL_REDUCED);
+  // Init buffers
+  // Register message handlers
+//  app_message_register_inbox_received(in_received_handler);
+////   app_message_register_inbox_dropped(in_dropped_handler);
+ //  app_message_register_outbox_sent(out_sent_handler);
+ //  app_message_register_outbox_failed(out_failed_handler);
+
+     app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
 
   // Setup the window handlers
   window_set_window_handlers(window, (WindowHandlers) {
@@ -373,14 +545,7 @@ int main(void) {
   });
 
   window_stack_push(window, true /* Animated */);
-app_comm_set_sniff_interval(SNIFF_INTERVAL_REDUCED);
-  // Init buffers
-   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-  // Register message handlers
-  app_message_register_inbox_received(in_received_handler);
-   app_message_register_inbox_dropped(in_dropped_handler);
-   app_message_register_outbox_sent(out_sent_handler);
-   app_message_register_outbox_failed(out_failed_handler);
+
 
 
   app_event_loop();
